8f9898ea27ccacad39d9e100fefeb9ada644348b
Arisha-Akbar <arishaakbar713@gmail.com>
2026-08-08 15:17:02 +0500

    first commit

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import { app } from "./app";
+import connectDB from "./utils/db";
+require("dotenv").config();
+//create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
+});
